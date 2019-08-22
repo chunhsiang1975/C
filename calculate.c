47865908ca8fa38a24856f5224a586587eeefa65
chunhsiang1975 <54344440+chunhsiang1975@users.noreply.github.com>
2019-08-22 09:09:38 +0800

    Update calculate.c

--- calculate.c
+++ calculate.c
@@ -3,44 +3,35 @@ int main(){
 	char op;
 	int a,b,c;
 	while(1){
-
 		printf("Please input operator\n");
 		scanf(" %c",&op);
-
-
 		if(op=='+'){
-		printf("Please input two numbers\n");
-		scanf("%i %i",&a,&b);
-		c=a+b;
-		printf("%d+%d=%d\n",a,b,c);
-		
+			printf("Please input two numbers\n");
+			scanf("%i %i",&a,&b);
+			c=a+b;
+			printf("%d+%d=%d\n",a,b,c);
 		}
 		else if (op=='-'){
-		printf("Please input two numbers\n");
-		scanf("%i %i",&a,&b);
-		c=a-b;
-		printf("%d-%d=%d\n",a,b,c);
-		
+			printf("Please input two numbers\n");
+			scanf("%i %i",&a,&b);
+			c=a-b;
+			printf("%d-%d=%d\n",a,b,c);
 		}
 		else if (op=='*'){
-		printf("Please input two numbers\n");
-		scanf("%i %i",&a,&b);
-		c=a*b;
-		printf("%d*%d=%d\n",a,b,c);
-		
+			printf("Please input two numbers\n");
+			scanf("%i %i",&a,&b);
+			c=a*b;
+			printf("%d*%d=%d\n",a,b,c);
 		}
 		else if (op=='/'){
-		printf("Please input two numbers\n");
-		scanf("%i %i",&a,&b);
-		c=a/b;
-		printf("%d/%d=%d\n",a,b,c);
-		
+			printf("Please input two numbers\n");
+			scanf("%i %i",&a,&b);
+			c=a/b;
+			printf("%d/%d=%d\n",a,b,c);
 		}
 		else if(op=='x'){
 			break;
-		
 		}
-
 	}
 	return 0;
 }
