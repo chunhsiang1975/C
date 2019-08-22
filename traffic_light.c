37b439eb410c5eacabaeedcc4639fc499cb92243
Chun-Hsiang Chao <chunhsiang2009@gmail.com>
2019-08-22 17:53:29 +0800

    Add files via upload

---
+++ traffic_light.c
@@ -0,0 +1,34 @@
+/*
+Traffic Light red yellow green
+20190822
+Writed by Chun-Hsiang Chao
+*/
+#include <stdio.h>
+int main(void){
+	int red_delay_time=10;
+	int green_delay_time=10;
+	int yellow_delay_time=2;
+	int i=0;
+	int state=0;
+	while(1){
+		if((state==0)&&(i<red_delay_time)){
+			printf("red %d\n",red_delay_time-i);
+			i++;
+			if(i==red_delay_time){ state++;i=0;}
+			sleep(1);
+		}
+		else if((state==1)&&(i<green_delay_time)){
+			printf("green %d\n",green_delay_time-i);
+			i++;
+			if(i==green_delay_time){ state++;i=0;}
+			sleep(1);
+		}
+		else if((state==2)&&(i<yellow_delay_time)){
+			printf("yellow %d\n",yellow_delay_time-i);
+			i++;
+			if(i==yellow_delay_time){ state=0;i=0;}
+			sleep(1);
+		}		
+	}
+}
+
