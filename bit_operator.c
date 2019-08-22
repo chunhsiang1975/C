6c6d34380fc0a90f56a48c833bd4b5316612ac37
Chun-Hsiang Chao <chunhsiang2009@gmail.com>
2019-08-22 17:07:50 +0800

    Add files via upload

--- bit_operator.c
+++ bit_operator.c
@@ -1,10 +1,11 @@
 /*
-Print number to 8bits binary number.
+Print number to some bits binary number.
 20190822
 Writed by Chun-Hsiang Chao
 */
 #include <stdio.h>
 #include <math.h>
+#define bits 16
 char* printf_binary(int number);
 int main(void){
 	int i;
@@ -13,24 +14,24 @@ int main(void){
 	//00011000 = 0x 18
 	char *binary;
 	binary=printf_binary(number);
-	for(i=0;i<8;i++) printf("%c ",binary[i]);
+	for(i=0;i<bits;i++) printf("%c ",binary[i]);
 	printf("\n");
 	//printf("%c %c\n",binary[3],binary[4]);
 	return 0;
 }
 char* printf_binary(int number){
-		char binary[8];
+		char binary[bits];
 		char *result;
 		int i=0;
 		//printf("%d\n",number);
 		while(1){
 			if(number%2==0){
-			 binary[7-i]=48;
+			 binary[bits-1-i]=48;
 			}
-			else binary[7-i]=49;
+			else binary[bits-1-i]=49;
 			number=(int)(number/2);
 			i++;
-			if(i==8) break;
+			if(i==bits) break;
 		}
 		result=binary;
 		return result;
