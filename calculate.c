3c897b3ef0c68a4cbf12c0b9d4a2dbbef58f6dec
chunhsiang1975 <54344440+chunhsiang1975@users.noreply.github.com>
2019-08-22 09:11:32 +0800

    Update calculate.c
    Be careful that there is a space in front of %c.

--- calculate.c
+++ calculate.c
@@ -4,7 +4,7 @@ int main(){
 	int a,b,c;
 	while(1){
 		printf("Please input operator\n");
-		scanf(" %c",&op);
+		scanf(" %c",&op);//There is a space in front of %c.
 		if(op=='+'){
 			printf("Please input two numbers\n");
 			scanf("%i %i",&a,&b);
