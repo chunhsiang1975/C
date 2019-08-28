c16828797f55b270eea87564a714ffa6b0f01565
Chun-Hsiang Chao <chunhsiang2009@gmail.com>
2019-08-28 11:32:38 +0800

    Add files via upload

--- find_max_min.c
+++ find_max_min.c
@@ -11,17 +11,15 @@ int main(void){
     int i;
     int max,min;
     srand(time(0));
-    for(i=0; i<array_number; i++)
-        a[i] = rand();
+    for(i=0; i<array_number; i++) a[i] = rand();
     for(i=0; i<array_number; i++) printf("%d ", a[i]);		
 	printf("\n");
 	max=a[0];
 	min=a[array_number-1];
 	i=1;
 	while(1){
-		if(a[i]<min) min=a[i];
+		(a[i]<min)?min=a[i]:i+0x00;//if(a[i]<min) min=a[i];
 		(a[i]>max)?max=a[i]:i++;
-		
 		if(i==array_number) break;
 	}
 	printf("max=%d min=%d\n",max,min);
