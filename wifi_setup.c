e8383a3e7c3f10510965c9e520956e7c372719e7
Chun-Hsiang Chao <chunhsiang2009@gmail.com>
2019-09-25 17:40:16 +0800

    Add files via upload

---
+++ wifi_setup.c
@@ -0,0 +1,33 @@
+/*
+ *  Insert ssid and passwd to wifi config file
+ *  Written by Chun-Hsiang Chao
+ *  Date::20190925
+ */
+
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<stdlib.h>
+
+int main(int argc,char *argv[]){
+	FILE *out;
+	char config_file[100]="/etc/wpa_supplicant/wpa_supplicant.conf";
+	char *ssid;
+	char *passwd;
+	ssid=argv[1];
+	passwd=argv[2];
+	if((argv[1]!=NULL)&&(argv[2]!=NULL)){
+		out=fopen(config_file,"a");
+		fprintf(out,"network={\n");
+		fprintf(out,"\tssid=\"%s\"\n",ssid);
+		fprintf(out,"\tpsk=\"%s\"\n",passwd);
+		fprintf(out,"}\n");
+		fclose(out);
+		system("wpa_cli -i wlan0 reconfigure");	
+		printf("finished\n");
+	}
+	else{
+		printf("ssid or passwd error\n");
+	}
+	return 1;
+}
