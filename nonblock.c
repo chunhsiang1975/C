e0bbb12d294fabb347c249cf871b853d66f53c78
Chun-Hsiang Chao <chunhsiang2009@gmail.com>
2019-10-07 09:38:31 +0800

    Add files via upload

---
+++ nonblock.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    char buf[100];
+    int flags;
+
+    flags = fcntl(0, F_GETFL);  	/* 0:stdin */
+    flags |= O_NONBLOCK;	  		/* Enable O_NONBLOCK bit */
+    fcntl(0, F_SETFL, flags);
+
+    while(1)
+    {
+        sleep(1);
+
+        if(read(0, buf, 100)>0)
+            printf("got input\n");
+        else
+            printf("no input\n");
+    }
+
+    return 0;
+}
+
