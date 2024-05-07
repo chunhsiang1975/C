d0b175b2d9344434cf39454da05d34713c615e60
Chun-Hsiang Chao <chunhsiang2009@gmail.com>
2024-05-07 16:45:44 +0800

    Add files via upload

--- socket_server.c
+++ socket_server.c
@@ -10,13 +10,13 @@
 #define PORT 8081
 int main(int argc, char const* argv[]){
 	int server_fd, new_socket;
-	ssize_t valread;
+	int valread;
 	struct sockaddr_in address;
 	int opt = 1;
 	socklen_t addrlen = sizeof(address);
 	char buffer[1024] = { 0 };
 	char* hello = "Hello from server";
-
+	printf("%s %ld\n",buffer,strlen(buffer));
 	//int sockfd = socket(domain, type, protocol) 
 	//domain: integer, specifies communication domain. We use AF_ LOCAL as defined in the POSIX standard for communication between processes on the same host. For communicating between processes on different hosts connected by IPV4, we use AF_INET and AF_I NET 6 for processes connected by IPV6.
 	//AF_INET is the Internet address family for IPv4. SOCK_STREAM is the socket type for TCP, the protocol that will be used to transport messages in the network.
@@ -45,28 +45,45 @@ int main(int argc, char const* argv[]){
 
 	printf("%d %d %d\n",AF_INET,address.sin_addr.s_addr,address.sin_port);
 	// Forcefully attaching socket to the port 8081
-//20240503
 	//int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+	//It is normally necessary to assign a local address using bind() before a SOCK_STREAM socket may receive connections
 	if (bind(server_fd, (struct sockaddr*)&address, sizeof(address))< 0) {
 		perror("bind failed");
 		exit(EXIT_FAILURE);
 	}
+	//int listen(int socket, int backlog);
+	//backlog Defines the maximum length for the queue of pending connections.
 	if (listen(server_fd, 3) < 0) {
 		perror("listen");
 		exit(EXIT_FAILURE);
 	}
-	if ((new_socket = accept(server_fd, (struct sockaddr*)&address, &addrlen)) < 0) {
-		perror("accept");
-		exit(EXIT_FAILURE);
-	}
-	valread = read(new_socket, buffer, 1024 - 1); // subtract 1 for the null
-							// terminator at the end
-	printf("%s\n", buffer);
-	send(new_socket, hello, strlen(hello), 0);
-	printf("Hello message sent\n");
+
+	while(1){
+		printf("%d\n",__LINE__);
+	//int accept(int socket, struct sockaddr *restrict address, socklen_t *restrict address_len);
+		if ((new_socket = accept(server_fd, (struct sockaddr*)&address, &addrlen)) < 0) {
+			perror("accept");
+			exit(EXIT_FAILURE);
+		}
+		printf("%d\n",__LINE__);
+		//int read(int socket, void *buf, int N);
+		//buf : The pointer to the buffer that receives the data.
+		//N : The length in bytes of the buffer pointed to by the buf parameter.
+		valread = read(new_socket, buffer, 1024 - 1); // buffer's size is 1024. subtract 1 for the null. 
+		// terminator at the end
+		printf("%s\n", buffer);
+		//int write(int fs, const void *buf, int N);
+		write(new_socket, hello, strlen(hello));
+		printf("Hello message sent\n");
+		//int send(int socket, char *buffer, int length, int flags);
+		//flags : The flags parameter is set by If more than one flag is specified, the logical OR operator (|) must be used to separate them.
+		sprintf(buffer,"send command from server.");
+		send(new_socket, buffer, strlen(buffer), 0);
+		printf("send command message sent\n");
 
 	// closing the connected socket
-	close(new_socket);
+		close(new_socket);
+	}
 	// closing the listening socket
 	close(server_fd);
 	return 0;
