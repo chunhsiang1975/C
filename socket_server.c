3dc18d401e706ff8fd155b6fac48900394618bab
Chun-Hsiang Chao <chunhsiang2009@gmail.com>
2024-05-08 16:46:48 +0800

    Add files via upload

--- socket_server.c
+++ socket_server.c
@@ -39,8 +39,9 @@ int main(int argc, char const* argv[]){
 		exit(EXIT_FAILURE);
 	}
 	address.sin_family = AF_INET;
-	//address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_addr.s_addr = inet_addr("127.0.0.1");
+	address.sin_addr.s_addr = INADDR_ANY;
+	//address.sin_addr.s_addr = inet_addr("127.0.0.1");
+	//inet_pton(AF_INT,"127.0.0.1",&address.sin_addr.s_addr);
 	address.sin_port = htons(PORT);
 
 	printf("%d %d %d\n",AF_INET,address.sin_addr.s_addr,address.sin_port);
